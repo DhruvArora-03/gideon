5cf46bb80e5d52961eca9144351a40551a5a77d1
Dhruv Arora <dhruvarora.us@gmail.com>
2025-01-21 09:25:27 -0600

    add loading state

--- web-svelte/src/lib/components/InviteUser.svelte
+++ web-svelte/src/lib/components/InviteUser.svelte
@@ -11,6 +11,7 @@
   import { Control, Description, Field, FieldErrors, Label } from '$lib/components/ui/form';
   import { superForm, type Infer, type SuperValidated } from 'sveltekit-superforms';
   import { inviteFormSchema } from '$lib/validation';
+  import { LoaderCircle, Send } from 'lucide-svelte';
   import { zodClient } from 'sveltekit-superforms/adapters';
 
   type Props = {
@@ -22,7 +23,7 @@
     validators: zodClient(inviteFormSchema),
   });
 
-  const { form: formData, message, enhance } = form;
+  const { form: formData, message, enhance, submitting } = form;
 </script>
 
 <Card class="max-w-md">
@@ -38,7 +39,12 @@
         <Control let:attrs>
           <Label>
             First Name
-            <Input {...attrs} bind:value={$formData.first_name} placeholder="John" />
+            <Input
+              {...attrs}
+              bind:value={$formData.first_name}
+              placeholder="John"
+              disabled={$submitting}
+            />
           </Label>
         </Control>
         <Description class="sr-only">The new user's first name</Description>
@@ -48,7 +54,12 @@
         <Control let:attrs>
           <Label>
             Last Name
-            <Input {...attrs} bind:value={$formData.last_name} placeholder="Doe" />
+            <Input
+              {...attrs}
+              bind:value={$formData.last_name}
+              placeholder="Doe"
+              disabled={$submitting}
+            />
           </Label>
         </Control>
         <Description class="sr-only">The new user's last name</Description>
@@ -58,7 +69,12 @@
         <Control let:attrs>
           <Label>
             Email
-            <Input {...attrs} bind:value={$formData.email} placeholder="johndoe@example.com" />
+            <Input
+              {...attrs}
+              bind:value={$formData.email}
+              placeholder="johndoe@example.com"
+              disabled={$submitting}
+            />
           </Label>
         </Control>
         <Description class="sr-only">
@@ -70,13 +86,24 @@
         <Control let:attrs>
           <Label>
             Phone Number
-            <Input {...attrs} bind:value={$formData.phone_number} placeholder="555-555-5555" />
+            <Input
+              {...attrs}
+              bind:value={$formData.phone_number}
+              placeholder="555-555-5555"
+              disabled={$submitting}
+            />
           </Label>
         </Control>
         <Description class="sr-only">The new user's phone number</Description>
         <FieldErrors />
       </Field>
-      <Button class="mt-4 w-full" type="submit">Send Invite</Button>
+      <Button class="mt-4 w-full" type="submit" disabled={$submitting}>
+        {#if $submitting}
+          <LoaderCircle class="mr-2 h-4 w-4 animate-spin" /> Sending Invite...
+        {:else}
+          <Send class="mr-2 h-4 w-4" /> Send Invite
+        {/if}
+      </Button>
       <p class="mt-4">
         Message: {$message}
       </p>
