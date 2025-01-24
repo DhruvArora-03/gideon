8197d18654253471ec36077ce0022ff297dc64c7
Dhruv Arora <dhruvarora.us@gmail.com>
2025-01-24 01:26:38 -0600

    comment extra zod schema

--- web-svelte/src/lib/validation.ts
+++ web-svelte/src/lib/validation.ts
@@ -21,20 +21,20 @@ export const loginFormSchema = z
   .required();
 export type LoginFormSchema = z.infer<typeof loginFormSchema>;
 
-export const resetPasswordFormSchema = z
-  .object({
-    new_password: z
-      .string()
-      .min(8, 'Password must be at least 8 characters long')
-      .regex(/[A-Z]/, 'Password must contain at least one uppercase letter')
-      .regex(/[a-z]/, 'Password must contain at least one lowercase letter')
-      .regex(/[0-9]/, 'Password must contain at least one number')
-      .regex(/[!@#$%^&*(),.?":{}|<>]/, 'Password must contain at least one special character'),
-    confirm_new_password: z.string(),
-  })
-  .required()
-  .refine((data) => data.new_password === data.confirm_new_password, {
-    message: 'Passwords must match',
-    path: ['confirm_new_password'], // This specifies where the error will appear
-  });
-export type ResetPasswordFormSchema = z.infer<typeof resetPasswordFormSchema>;
+// export const resetPasswordFormSchema = z
+//   .object({
+//     new_password: z
+//       .string()
+//       .min(8, 'Password must be at least 8 characters long')
+//       .regex(/[A-Z]/, 'Password must contain at least one uppercase letter')
+//       .regex(/[a-z]/, 'Password must contain at least one lowercase letter')
+//       .regex(/[0-9]/, 'Password must contain at least one number')
+//       .regex(/[!@#$%^&*(),.?":{}|<>]/, 'Password must contain at least one special character'),
+//     confirm_new_password: z.string(),
+//   })
+//   .required()
+//   .refine((data) => data.new_password === data.confirm_new_password, {
+//     message: 'Passwords must match',
+//     path: ['confirm_new_password'], // This specifies where the error will appear
+//   });
+// export type ResetPasswordFormSchema = z.infer<typeof resetPasswordFormSchema>;
