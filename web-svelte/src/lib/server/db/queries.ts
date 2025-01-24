afd9c92d6996229f570cfd1830c0e60e192fdc28
Dhruv Arora <dhruvarora.us@gmail.com>
2025-01-24 12:47:13 -0600

    fix getSlots query

--- web-svelte/src/lib/server/db/queries.ts
+++ web-svelte/src/lib/server/db/queries.ts
@@ -1,9 +1,19 @@
 import { db } from '$lib/server/db';
-import { slots } from '$lib/server/db/schema';
-import { gte } from 'drizzle-orm';
+import { type SlotWithAssignments } from '$lib/server/db/schema';
 
-export async function getSlots() {
-  return await db.query.slots.findMany({
-    where: gte(slots.date, new Date().toISOString()),
+export async function getSlots(): Promise<SlotWithAssignments[]> {
+  const slots = await db.query.slots.findMany({
+    with: {
+      assignments: true,
+    },
+    where: (slots, { gt }) => gt(slots.start_time, new Date()),
   });
+
+  return slots.map((slot) => ({
+    ...slot,
+    // find unique assignments by user_id
+    assignments: slot.assignments
+      .sort((a, b) => b.created_at.getTime() - a.created_at.getTime())
+      .filter((curr, i, sorted) => sorted.findIndex((a) => a.user_id === curr.user_id) === i),
+  }));
 }
