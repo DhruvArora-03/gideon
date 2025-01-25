32749f46a82ceb28f685a54e6db25c66ce165ea5
Dhruv Arora <dhruvarora.us@gmail.com>
2025-01-24 21:36:07 -0600

    update date formats

--- web-svelte/src/lib/date.ts
+++ web-svelte/src/lib/date.ts
@@ -1,11 +1,20 @@
-export const dateFormat = new Intl.DateTimeFormat('en-US', {
-  weekday: 'short',
+const dateFormat = new Intl.DateTimeFormat('en-US', {
   month: 'short',
   day: '2-digit',
 });
 
-export const timeFormat = new Intl.DateTimeFormat('en-US', {
-  hour: 'numeric',
-  minute: '2-digit',
-  hour12: true,
+export const formatDate = dateFormat.format;
+
+const dateWithWeekdayFormat = new Intl.DateTimeFormat('en-US', {
+  weekday: 'short',
+  month: 'short',
+  day: 'numeric',
 });
+
+export const formatDateWithWeekday = dateWithWeekdayFormat.format;
+
+const timeFormat = new Intl.DateTimeFormat('en-US', {
+  hour: 'numeric',
+});
+
+export const formatTime = timeFormat.format;
