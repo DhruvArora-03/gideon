5942d0391ab2bd2e61df99b43e303cebfe6250f5
Dhruv Arora <dhruvarora.us@gmail.com>
2025-01-28 02:27:15 -0600

    dummy settings page

---
+++ web-svelte/src/routes/(authed)/settings/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import LogoutButton from '$lib/components/LogoutButton.svelte';
+  import type { PageData } from './$types';
+
+  let { supabase }: PageData = $props();
+</script>
+
+<h1>Settings</h1>
+<LogoutButton {supabase} />
