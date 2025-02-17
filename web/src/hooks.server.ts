2233694e00f558082ce410e084d0211bbe195aa1
Dhruv Arora <dhruvarora.us@gmail.com>
2025-02-17 10:00:31 -0600

    Revert "use db url instead?"
    This reverts commit 5fd378bcf5efab652a08fdbb5f8c517d3648ad52.

--- web/src/hooks.server.ts
+++ web/src/hooks.server.ts
@@ -2,8 +2,7 @@ import { createServerClient } from '@supabase/ssr';
 import { type Handle, redirect } from '@sveltejs/kit';
 import { sequence } from '@sveltejs/kit/hooks';
 
-import { PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
-import { DATABASE_URL } from '$env/static/private';
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
 
 const supabase: Handle = async ({ event, resolve }) => {
   /**
@@ -11,7 +10,7 @@ const supabase: Handle = async ({ event, resolve }) => {
    *
    * The Supabase client gets the Auth token from the request cookies.
    */
-  event.locals.supabase = createServerClient(DATABASE_URL, PUBLIC_SUPABASE_ANON_KEY, {
+  event.locals.supabase = createServerClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, {
     cookies: {
       getAll: () => event.cookies.getAll(),
       /**
