2c82ca1acb6958d788cfebd5c2ae897694873eab
Dhruv Arora <dhruvarora.us@gmail.com>
2025-03-18 11:30:15 -0500

    blank manage users page

---
+++ web/src/routes/(authed)/admin/users/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import PageWrapper from '$lib/components/PageWrapper.svelte';
+</script>
+
+<PageWrapper class="max-w-md space-y-4 p-4">
+  <h1 class="text-2xl font-semibold">Manage Users</h1>
+</PageWrapper>
