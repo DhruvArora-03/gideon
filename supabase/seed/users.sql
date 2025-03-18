df60840a114faa045b09a903069eb50f1bf19a44
Dhruv Arora <dhruvarora.us@gmail.com>
2025-03-18 11:05:12 -0500

    update user seed script to include managers and admins

--- supabase/seed/users.sql
+++ supabase/seed/users.sql
@@ -1,4 +1,4 @@
--- create test users
+-- create test employee accounts
 insert into
     auth.users (
         instance_id,
@@ -21,16 +21,16 @@ insert into
     ) (
         select
             '00000000-0000-0000-0000-000000000000',
-            lpad(row_number() over ()::text, 32, '0')::uuid,
+            lpad(n::text, 32, '0')::uuid,
             'authenticated',
             'authenticated',
-            'user' || (row_number() over ()) || '@example.com',
+            'user' || row_number() over () || '@example.com',
             crypt ('Password123!', gen_salt ('bf')),
             current_timestamp,
             current_timestamp,
             current_timestamp,
             '{"provider":"email","providers":["email"]}',
-            format('{"first_name":"%s","last_name":"%s","phone_number":"123-456-7890","role":"employee"}', 'First' || row_number() over (), 'Last' || row_number() over ())::jsonb,
+            format('{"first_name":"%s","last_name":"%s","phone_number":"123-456-7890","role":"employee"}', 'First' || n, 'Last' || n)::jsonb,
             current_timestamp,
             current_timestamp,
             '',
@@ -38,7 +38,93 @@ insert into
             '',
             ''
         from
-            generate_series(1, 10)
+            generate_series(1, 10) n
+    );
+
+-- create test manager accounts
+insert into
+    auth.users (
+        instance_id,
+        id,
+        aud,
+        role,
+        email,
+        encrypted_password,
+        email_confirmed_at,
+        recovery_sent_at,
+        last_sign_in_at,
+        raw_app_meta_data,
+        raw_user_meta_data,
+        created_at,
+        updated_at,
+        confirmation_token,
+        email_change,
+        email_change_token_new,
+        recovery_token
+    ) (
+        select
+            '00000000-0000-0000-0000-000000000000',
+            lpad(n::text, 32, '0')::uuid,
+            'authenticated',
+            'authenticated',
+            'manager' || row_number() over () || '@example.com',
+            crypt ('Password123!', gen_salt ('bf')),
+            current_timestamp,
+            current_timestamp,
+            current_timestamp,
+            '{"provider":"email","providers":["email"]}',
+            format('{"first_name":"%s","last_name":"%s","phone_number":"123-456-7890","role":"manager"}', 'First' || n, 'Last' || n)::jsonb,
+            current_timestamp,
+            current_timestamp,
+            '',
+            '',
+            '',
+            ''
+        from
+            generate_series(11, 20) n
+    );
+
+-- create test admin accounts
+insert into
+    auth.users (
+        instance_id,
+        id,
+        aud,
+        role,
+        email,
+        encrypted_password,
+        email_confirmed_at,
+        recovery_sent_at,
+        last_sign_in_at,
+        raw_app_meta_data,
+        raw_user_meta_data,
+        created_at,
+        updated_at,
+        confirmation_token,
+        email_change,
+        email_change_token_new,
+        recovery_token
+    ) (
+        select
+            '00000000-0000-0000-0000-000000000000',
+            lpad(n::text, 32, '0')::uuid,
+            'authenticated',
+            'authenticated',
+            'admin' || row_number() over () || '@example.com',
+            crypt ('Password123!', gen_salt ('bf')),
+            current_timestamp,
+            current_timestamp,
+            current_timestamp,
+            '{"provider":"email","providers":["email"]}',
+            format('{"first_name":"%s","last_name":"%s","phone_number":"123-456-7890","role":"admin"}', 'First' || n, 'Last' || n)::jsonb,
+            current_timestamp,
+            current_timestamp,
+            '',
+            '',
+            '',
+            ''
+        from
+            generate_series(21, 30) n
     );
 
 -- test user email identities
