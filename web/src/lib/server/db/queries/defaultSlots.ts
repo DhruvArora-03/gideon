ec3ab2a6861ccfa7b2d516af09491d856ee9cba2
Dhruv Arora <dhruvarora.us@gmail.com>
2025-04-05 17:54:03 -0500

    define db functions for default slots

--- web/src/lib/server/db/queries/defaultSlots.ts
+++ web/src/lib/server/db/queries/defaultSlots.ts
@@ -1,8 +1,23 @@
 import { db } from '$lib/server/db';
-import type { DefaultSlot } from '$lib/server/db/schema';
+import { defaultSlots, type DefaultSlot, type NewDefaultSlot } from '$lib/server/db/schema';
+import { eq } from 'drizzle-orm';
+
+type DefaultSlotMutation = Omit<NewDefaultSlot, 'id' | 'created_at' | 'updated_at'>;
 
 export async function getDefaultSlots(): Promise<DefaultSlot[]> {
   return await db.query.defaultSlots.findMany({
     orderBy: (defaultSlots) => defaultSlots.dotw,
   });
+}
+
+export async function createDefaultSlot(params: DefaultSlotMutation): Promise<void> {
+  await db.insert(defaultSlots).values(params);
+}
+
+export async function updateDefaultSlot(id: number, params: DefaultSlotMutation): Promise<void> {
+  await db.update(defaultSlots).set(params).where(eq(defaultSlots.id, id));
+}
+
+export async function deleteDefaultSlot(id: number): Promise<void> {
+  await db.delete(defaultSlots).where(eq(defaultSlots.id, id));
 }
