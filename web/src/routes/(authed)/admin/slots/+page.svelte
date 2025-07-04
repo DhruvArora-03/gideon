0799788f5781ec344c55f6f3561159d7088fa8bd
Dhruv Arora <dhruvarora.us@gmail.com>
2025-07-05 00:38:22 +0530

    update slot management UI

--- web/src/routes/(authed)/admin/slots/+page.svelte
+++ web/src/routes/(authed)/admin/slots/+page.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
   import PageWrapper from '$lib/components/PageWrapper.svelte';
+  import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+  } from '$lib/components/ui/card';
   import { Table, TableBody, TableCell, TableHead, TableRow } from '$lib/components/ui/table';
   import TableHeader from '$lib/components/ui/table/table-header.svelte';
   import { formatDateWithWeekday, formatTime } from '$lib/date';
@@ -11,50 +18,51 @@
   const { data }: Props = $props();
 </script>
 
-<PageWrapper class="space-y-4 p-4">
-  <h1 class="text-2xl font-semibold">Manage Slots</h1>
-  {#await data.slots}
-    loading...
-  {:then slots}
-    <Table>
-      <TableHeader>
-        <TableHead>id</TableHead>
-        <TableHead>date</TableHead>
-        <TableHead>start</TableHead>
-        <TableHead>end</TableHead>
-        <TableHead># Signups</TableHead>
-        <TableHead># Waitlist</TableHead>
-        <TableHead>cap</TableHead>
-      </TableHeader>
-      <TableBody>
-        {#each slots as s}
-          <TableRow>
-            <TableCell>
-              {s.id}
-            </TableCell>
-            <TableCell>
-              {formatDateWithWeekday(s.start_time)}
-            </TableCell>
-            <TableCell>
-              {formatTime(s.start_time)}
-            </TableCell>
-            <TableCell>
-              {formatTime(s.end_time)}
-            </TableCell>
-            <TableCell>
-              {s.assignments.filter((a) => a.assignment_status === 'confirmed').length}
-            </TableCell>
-            <TableCell>
-              {s.assignments.filter((a) => a.assignment_status === 'waitlisted').length}
-            </TableCell>
-            <TableCell>
-              {s.capacity}
-            </TableCell>
-          </TableRow>
-        {/each}
-      </TableBody>
-    </Table>
-  {:catch e}
-    {e.message}
-  {/await}
+<PageWrapper class="mx-auto max-w-md">
+  <Card class="m-4">
+    <CardHeader>
+      <CardTitle class="text-2xl">Manage Slots</CardTitle>
+      <CardDescription>
+        Data of all upcoming slots. Counts is (SF/WL/ES).
+        <br />
+        - SF: Spots Filled
+        <br />
+        - WL: Waitlisted
+        <br />
+        - ES: Empty Slots
+      </CardDescription>
+    </CardHeader>
+    <CardContent>
+      {#await data.slots}
+        loading...
+      {:then slots}
+        <Table>
+          <TableHeader>
+            <TableHead>Date</TableHead>
+            <TableHead>Start</TableHead>
+            <TableHead>End</TableHead>
+            <TableHead>Counts</TableHead>
+          </TableHeader>
+          <TableBody>
+            {#each slots as s}
+              {@const signups = s.assignments.filter(
+                (a) => a.assignment_status === 'confirmed',
+              ).length}
+              {@const waitlisted = s.assignments.filter(
+                (a) => a.assignment_status === 'waitlisted',
+              ).length}
+              <TableRow>
+                <TableCell>{formatDateWithWeekday(s.start_time)}</TableCell>
+                <TableCell>{formatTime(s.start_time)}</TableCell>
+                <TableCell>{formatTime(s.end_time)}</TableCell>
+                <TableCell>{signups}/{waitlisted}/{s.capacity - signups - waitlisted}</TableCell>
+              </TableRow>
+            {/each}
+          </TableBody>
+        </Table>
+      {:catch e}
+        {e.message}
+      {/await}
+    </CardContent>
+  </Card>
 </PageWrapper>
