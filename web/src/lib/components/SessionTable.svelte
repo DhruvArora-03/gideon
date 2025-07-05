6c3c1cc8d05d11f0ea7368242aacf2e5cb4276d2
Dhruv Arora <dhruvarora.us@gmail.com>
2025-07-05 19:58:10 +0530

    update tooltip

--- web/src/lib/components/SessionTable.svelte
+++ web/src/lib/components/SessionTable.svelte
@@ -42,16 +42,19 @@
         <TableCell>{formatDateWithWeekday(s.clock_in)}</TableCell>
         <TableCell>{formatTime(s.clock_in)}</TableCell>
         <TableCell>{s.clock_out ? formatTime(s.clock_out) : ''}</TableCell>
-        <TableCell>{s.clock_out ? getDuration(s.clock_in, s.clock_out) : ''}</TableCell>
-        <TableCell>
+        <TableCell class="flex flex-row gap-2">
+          <div>
+            {s.clock_out ? getDuration(s.clock_in, s.clock_out) : ''}
+          </div>
+
           {#if !s.was_scheduled}
             <TooltipProvider>
-              <Tooltip delayDuration={0}>
-                <TooltipTrigger class={buttonVariants({ variant: 'ghost' })}>
-                  <TriangleAlertIcon size={16} />
+              <Tooltip delayDuration={100} disableCloseOnTriggerClick>
+                <TooltipTrigger>
+                  <TriangleAlertIcon class="text-destructive" size={16} />
                 </TooltipTrigger>
                 <TooltipContent>
-                  <p>Add to library</p>
+                  <p>Not on schedule for this shift</p>
                 </TooltipContent>
               </Tooltip>
             </TooltipProvider>
