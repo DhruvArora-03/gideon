5a1c780024d83d24ee35dd1e1c9f545ec1c3ad9b
Dhruv Arora <dhruvarora.us@gmail.com>
2025-07-06 18:39:17 +0530

    mass prettier fix and remove unnecessary utils

--- web/src/routes/(authed)/admin/slots/+page.server.ts
+++ web/src/routes/(authed)/admin/slots/+page.server.ts
@@ -1,5 +1,5 @@
-import queries from "$lib/server/db/queries";
-import type { PageServerLoad } from "./$types";
+import queries from '$lib/server/db/queries';
+import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
   return {
